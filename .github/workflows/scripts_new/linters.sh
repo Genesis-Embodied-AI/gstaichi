0aed554e74693698ea25fcc5edd0f0008234861f
Hugh Perkins <hughperkins@gmail.com>
2025-06-03 10:47:54 -0700

    run pyright on modifeid files

--- .github/workflows/scripts_new/linters.sh
+++ .github/workflows/scripts_new/linters.sh
@@ -16,10 +16,12 @@ pre-commit run -a --show-diff
 # python
 # ======
 
-pip install mypy
-# FIXME: Need to deal with all the ti.template() and similar, which
-# violate typing/myp rules. And then we can run:
-# mypy python
+pip install pyright
+# since runnign pyright on everything is chaos right now,
+# we only run it on modified files
+# we factorize this to a separate script, so people can run it
+# directly if they wish
+bash .github/workflows/scripts_new/run_pyright_changed_files.sh
 
 pip install isort
 # TODO: run isort on all python files, and commit those, then
