38e821b4732c5e0693ad984bdd5c0a016ecf240a
Hugh Perkins <hughperkins@gmail.com>
2025-06-04 09:34:45 -0700

    remove dead code

--- .github/workflows/scripts_new/win.ps1
+++ .github/workflows/scripts_new/win.ps1
@@ -11,12 +11,6 @@ python --version
 git submodule update --init --recursive
 
 Start-Process -NoNewWindow -FilePath "python" -ArgumentList "build.py" -ErrorAction SilentlyContinue -Wait
-# try {
-#     python myscript.py
-#     if ($LASTEXITCODE -ne 0) { throw "Python script failed with exit code $LASTEXITCODE" }
-# } catch {
-#     Write-Host "Python script failed, but we're continuing anyway: $_"
-# }
 python build.py
 pip install .\dist\taichi-1.8.0-cp310-cp310-win_amd64.whl
 
