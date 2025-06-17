d7bcaecc28b63f7761dca26d6fbc7ab0cb74684b
Hugh Perkins <hughperkins@gmail.com>
2025-06-16 18:27:31 -0700

    ruff check

--- .github/workflows/scripts_new/linters.sh
+++ .github/workflows/scripts_new/linters.sh
@@ -16,14 +16,8 @@ pre-commit run -a --show-diff
 # python
 # ======
 
-pip install pyright
-# Need to deal with C++ linkage issues first (and possibly
-# some other things), before we can turn on pyright
-
-pip install isort
-# TODO: run isort on all python files, and commit those, then
-# uncomment the following line:
-# isort --check-only --diff python
+pip install ruff
+ruff check
 
 # C++
 # ===
