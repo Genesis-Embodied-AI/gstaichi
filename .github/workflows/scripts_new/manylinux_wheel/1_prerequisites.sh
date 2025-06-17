22b78be6f0bb36b8223bfd5f2a03bdd2f5740631
Hugh Perkins <hughperkins@gmail.com>
2025-06-17 15:44:29 -0700

    make w safe

--- .github/workflows/scripts_new/manylinux_wheel/1_prerequisites.sh
+++ .github/workflows/scripts_new/manylinux_wheel/1_prerequisites.sh
@@ -4,5 +4,5 @@ set -ex
 
 yum update
 yum install -y git clang libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
+git config --global --add safe.directory /__w/taichi/taichi
 git submodule update --init --jobs 2
-
