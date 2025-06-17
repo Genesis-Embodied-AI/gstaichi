ae1552b6c6bd4abb64d6cf0d095fbe61633c72d3
Hugh Perkins <hughperkins@gmail.com>
2025-06-16 18:46:05 -0700

    move ruff check into pre-commit

--- .github/workflows/scripts_new/linters.sh
+++ .github/workflows/scripts_new/linters.sh
@@ -7,19 +7,5 @@ pwd
 ls
 uname -a
 
-# python + C++
-# =============
-
 pip install pre-commit
 pre-commit run -a --show-diff
-
-# python
-# ======
-
-pip install ruff
-ruff check
-
-# C++
-# ===
-
-# TODO: figure out how to run clang-tidy
