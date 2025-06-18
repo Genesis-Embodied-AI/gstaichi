7eb1cb7d3ea1371998102c33043f3fe54c8220a4
Hugh Perkins <hughperkins@gmail.com>
2025-06-17 20:07:35 -0700

    try different wheel path

--- .github/workflows/scripts_new/manylinux_wheel/4_install_wheel.sh
+++ .github/workflows/scripts_new/manylinux_wheel/4_install_wheel.sh
@@ -6,10 +6,11 @@ ls -l
 find . -cmin -1
 find . -cmin -1 -name '*.whl
 find . -name '*.whl
+ls -lh *.whl
 
 python -V
 pip -V
 
-pip install manylinux_wheel/dist/*.whl
+pip install *.whl
 
 python -c 'import taichi as ti; ti.init(arch=ti.cpu); print(ti.__version__)'
