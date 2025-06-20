a62d935f00ecfa4c28855dbc293829ac31125670
Hugh Perkins <hughperkins@gmail.com>
2025-06-19 17:48:47 -0700

    precommit

--- tests/python/test_template_typing.py
+++ tests/python/test_template_typing.py
@@ -3,9 +3,11 @@ import pytest
 import taichi as ti
 from tests import test_utils
 
+
 @ti.kernel
 def some_kernel(a: ti.template, b: ti.Template) -> None:
     a[None] = b[None] + 2
+
 
 @test_utils.test()
 def test_template_no_braces():
