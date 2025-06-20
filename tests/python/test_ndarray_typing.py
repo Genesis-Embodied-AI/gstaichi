5434b4a810b908bf5f53139edced8085c7ecd439
Hugh Perkins <hughperkins@gmail.com>
2025-06-19 17:49:19 -0700

    precommit

--- tests/python/test_ndarray_typing.py
+++ tests/python/test_ndarray_typing.py
@@ -3,10 +3,12 @@ import pytest
 import taichi as ti
 from tests import test_utils
 
+
 @ti.kernel
 def some_kernel(a: ti.types.NDArray[ti.i32, 2], b: ti.types.NDArray[ti.i32, 2]) -> None:
     for i, j in b:
         a[i, j] = b[i, j] + 2
+
 
 @test_utils.test()
 def test_ndarray_typing_square_brackets():
