f6828a705233930bfda69709b4af6432a89220fb
Hugh Perkins <hughperkins@gmail.com>
2025-06-26 21:18:00 -0700

    -q

--- .github/workflows/scripts_new/manylinux_wheel/1_prerequisites.sh
+++ .github/workflows/scripts_new/manylinux_wheel/1_prerequisites.sh
@@ -7,5 +7,5 @@ yum install -y git wget libXrandr-devel libXinerama-devel libXcursor-devel libXi
 git config --global --add safe.directory /__w/taichi/taichi
 git submodule update --init --jobs 2
 
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.4/clang+llvm-15.0.4-x86_64-linux-gnu-rhel-8.4.tar.xz
+wget -q https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.4/clang+llvm-15.0.4-x86_64-linux-gnu-rhel-8.4.tar.xz
 tar -xf clang+llvm-15.0.4-x86_64-linux-gnu-rhel-8.4.tar.xz
