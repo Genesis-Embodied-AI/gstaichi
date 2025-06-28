f558502ec8d7d9f102c4d793926a92382aad6f70
Hugh Perkins <hughperkins@gmail.com>
2025-06-28 12:06:32 -0700

    link libstdc++.so, so can be found easily inchecks

--- .github/workflows/scripts_new/manylinux_wheel/1_prerequisites.sh
+++ .github/workflows/scripts_new/manylinux_wheel/1_prerequisites.sh
@@ -9,3 +9,11 @@ git submodule update --init --jobs 2
 
 wget -q https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.4/clang+llvm-15.0.4-x86_64-linux-gnu-rhel-8.4.tar.xz
 tar -xf clang+llvm-15.0.4-x86_64-linux-gnu-rhel-8.4.tar.xz
+
+# clang++ searches for libstd++.so, not libstdc++.so.6
+# without this, then the compiler checks will fail
+# eg:
+# - check for working compiler itself
+# - and also check for -Wno-unused-but-set-variable, in TaichiCXXFlags.cmake
+#   which will cause obscure compile errors for external/Eigen
+ln -s /usr/lib64/libstdc++.so.6 /usr/lib64/libstdc++.so
