42a61d4df58e2885f8818d79f85d59d430321ae3
Hugh Perkins <hughperkins@gmail.com>
2025-06-29 12:43:00 -0700

    add test of typing

---
+++ tests/python/test_pyi_stubs.py
@@ -0,0 +1,24 @@
+from tests import test_utils
+import tempfile
+import sys
+import os
+import subprocess
+
+
+@test_utils.test()
+def test_pyi_stubs():
+    test_code = """
+import taichi._lib.core.taichi_python
+reveal_type(taichi._lib.core.taichi_python)
+"""
+    with tempfile.TemporaryDirectory() as tmpdir:
+        test_file = os.path.join(tmpdir, "tmp_mypy_test.py")
+        with open(test_file, "w") as f:
+            f.write(test_code)
+
+        # if no types, this will fail
+        subprocess.check_output([
+            sys.executable,
+            "-m", "mypy",
+            test_file,
+        ])
