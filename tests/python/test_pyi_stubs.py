30a7d054f906a390cdfd268596931be47f605ac1
Hugh Perkins <hughperkins@gmail.com>
2025-07-02 07:22:15 -0700

    address review comments

--- tests/python/test_pyi_stubs.py
+++ tests/python/test_pyi_stubs.py
@@ -1,28 +1,17 @@
-import os
 import subprocess
 import sys
-import tempfile
 
 from tests import test_utils
 
 
 @test_utils.test()
-def test_pyi_stubs():
+def test_pyi_stubs(tmpdir):
     test_code = """
 import taichi._lib.core.taichi_python
 reveal_type(taichi._lib.core.taichi_python)
 """
-    with tempfile.TemporaryDirectory() as tmpdir:
-        test_file = os.path.join(tmpdir, "tmp_mypy_test.py")
-        with open(test_file, "w") as f:
-            f.write(test_code)
+    test_file = tmpdir.join("tmp_mypy_test.py")
+    test_file.write(test_code)
 
-        res = subprocess.check_output(
-            [
-                sys.executable,
-                "-m",
-                "pyright",
-                test_file,
-            ]
-        ).decode("utf-8")
-        assert "unknown" not in res.lower()
+    res = subprocess.check_output([sys.executable, "-m", "pyright", test_file]).decode("utf-8")
+    assert "unknown" not in res.lower()
