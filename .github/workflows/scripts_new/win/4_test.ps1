72d4e67c972ba8db4bcf5699e4b9f53c819a5e45
Hugh Perkins <hughperkins@gmail.com>
2025-07-07 06:44:35 -0700

    Revert "try --prefer-binary"
    This reverts commit de9a984164bc0ae522d1f1447122bff84743e9ff.

--- .github/workflows/scripts_new/win/4_test.ps1
+++ .github/workflows/scripts_new/win/4_test.ps1
@@ -3,5 +3,5 @@ Set-PSDebug -Trace 1
 trap { Write-Error $_; exit 1 }
 
 python -c 'import taichi as ti; ti.init();'
-pip install --prefer-binary -r requirements_test.txt
+pip install -r requirements_test.txt
 python .\tests\run_tests.py -v -r 3
