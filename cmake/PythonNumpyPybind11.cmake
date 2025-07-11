34bfa0f78daa39852e906daaecb3fb1baac7f3ad
Hugh Perkins <hughperkins@gmail.com>
2025-07-11 12:32:51 -0700

    move pybind version into cmakelists

--- cmake/PythonNumpyPybind11.cmake
+++ cmake/PythonNumpyPybind11.cmake
@@ -12,10 +12,11 @@ include_directories(${NUMPY_INCLUDE_DIR})
 
 include(FetchContent)
 
+message("Using pybind11 version: ${PYBIND11_VERSION}")
 FetchContent_Declare(
   pybind11
   GIT_REPOSITORY https://github.com/pybind/pybind11.git
-  GIT_TAG v2.13.6  # Should be less than 3.0.0 for now
+  GIT_TAG v${PYBIND11_VERSION}  # Should be less than 3.0.0 for now
      # pybind11 3.0.0 caused builds to start failing. This PR downgrades pybind11 to latest <3.0.0 release,
      # which fixes the issue
      # whilst we could probably get 3.0.0 working too, I feel that x.0.0 releases, in opensource (and closed source
