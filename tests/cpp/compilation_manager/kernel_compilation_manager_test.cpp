ecdd408fa8f6c79f682c2a57bc794353e2789ad8
Hugh Perkins <hughperkins@gmail.com>
2025-08-05 16:25:44 -0400

    gstaichi

--- tests/cpp/compilation_manager/kernel_compilation_manager_test.cpp
+++ tests/cpp/compilation_manager/kernel_compilation_manager_test.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 #include "gtest/gtest.h"
-#include "taichi/common/core.h"
+#include "gstaichi/common/core.h"
 
 TEST(KernelCompilationManager, Basic) {
     std::cout << "KernelCompilationManager test started." << std::endl;
