a5d5cc958f909e7b00640ddb5d5266b20693d827
Hugh Perkins <hughperkins@gmail.com>
2025-08-12 12:23:33 -0400

    try new method to get TI_LIB_DIR

--- .github/workflows/scripts_new/macosx/4_test.sh
+++ .github/workflows/scripts_new/macosx/4_test.sh
@@ -5,7 +5,7 @@ set -ex
 pip install --prefer-binary -r requirements_test.txt
 find . -name '*.bc'
 ls -lh build/
-ls -lh python/gstaichi/_lib/runtime/
-export TI_LIB_DIR=python/gstaichi/_lib/runtime
+TI_LIB_DIR=$(python -c "import os; import gstaichi as ti; p = os.path.join(ti.__path__[0], '_lib', 'runtime'); print(p)" | tail -n 1)
+export TI_LIB_DIR
 ./build/gstaichi_cpp_tests
 python tests/run_tests.py -v -r 3
