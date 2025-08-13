568fc07890d7eeff22c360d33202dc16d987b65f
Hugh Perkins <hughperkins@gmail.com>
2025-08-13 13:36:33 -0400

    get ti lib dir from command

--- .github/workflows/scripts_new/linux_x86/4_test.sh
+++ .github/workflows/scripts_new/linux_x86/4_test.sh
@@ -3,6 +3,7 @@
 set -ex
 
 pip install -r requirements_test.txt
-export TI_LIB_DIR=python/gstaichi/_lib/runtime
+TI_LIB_DIR=$(python -c "import os; import gstaichi as ti; p = os.path.join(ti.__path__[0], '_lib', 'runtime'); print(p)" | tail -n 1)
+export TI_LIB_DIR
 ./build/gstaichi_cpp_tests
 python tests/run_tests.py -v -r 3
