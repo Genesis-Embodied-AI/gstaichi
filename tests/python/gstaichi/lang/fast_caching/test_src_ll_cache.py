50e6ee1fbdc6db45f6701e5b4b89f1c9fcdc54fb
Hugh Perkins <hughperkins@gmail.com>
2025-08-20 16:07:26 -0400

    third time lucky? (or is it forth or fifth time? 🤔)

--- tests/python/gstaichi/lang/fast_caching/test_src_ll_cache.py
+++ tests/python/gstaichi/lang/fast_caching/test_src_ll_cache.py
@@ -1,6 +1,9 @@
+import os
 import pathlib
 import sys
 from io import StringIO
+
+import pytest
 
 import gstaichi as ti
 from gstaichi._lib.core import gstaichi_python
@@ -41,14 +44,45 @@ def test_src_ll_cache1(tmp_path: pathlib.Path) -> None:
     assert has_pure._primal.src_ll_cache_observations.cache_loaded
 
 
+class StderrCapture:
+    def __init__(self, tmp_file):
+        self.tmp_file = tmp_file
+
+    def read(self):
+        self.tmp_file.flush()
+        self.tmp_file.seek(0)
+        out = self.tmp_file.read()
+        self.tmp_file.truncate(0)
+        self.tmp_file.seek(0)
+        return out
+
+
+@pytest.fixture
+def redirect_stderr(tmp_path):
+    """
+    This redirects the underlying operating system stderr. Since
+    we are calling into a c++ library that does writes to stderr,
+    then redirecting things only in python land is not enough.
+    Concretely, using capfd works on linux, and works for stdout
+    on Windows, but I couldn't get it working with stderr on Windows.
+    """
+    tmp_stderr_path = tmp_path / "stderr.txt"
+    with open(tmp_stderr_path, "w+") as tmp_stderr:
+        old_stderr_fd = os.dup(2)
+        os.dup2(tmp_stderr.fileno(), 2)
+        capture = StderrCapture(tmp_stderr)
+        try:
+            yield capture
+        finally:
+            os.dup2(old_stderr_fd, 2)
+            os.close(old_stderr_fd)
+
+
 # Should be enough to run these on cpu I think, and anything involving
 # stdout/stderr capture is fairly flaky on other arch
 @test_utils.test(arch=ti.cpu)
-def test_src_ll_cache_arg_warnings(tmp_path: pathlib.Path, monkeypatch) -> None:
+def test_src_ll_cache_arg_warnings(tmp_path: pathlib.Path, redirect_stderr) -> None:
     ti_init_same_arch(offline_cache_file_path=str(tmp_path), offline_cache=True)
-
-    stderr_capture = StringIO()
-    monkeypatch.setattr(sys, "stderr", stderr_capture)
 
     class RandomClass:
         pass
@@ -59,7 +93,7 @@ def test_src_ll_cache_arg_warnings(tmp_path: pathlib.Path, monkeypatch) -> None:
         pass
 
     k1(foo=RandomClass())
-    err = stderr_capture.getvalue()
+    err = redirect_stderr.read()
     assert "FASTCACHE_PARAM_INVALID" in err
     assert RandomClass.__name__ in err
     assert "FASTCACHE_INVALID_FUNC" in err
@@ -69,9 +103,8 @@ def test_src_ll_cache_arg_warnings(tmp_path: pathlib.Path, monkeypatch) -> None:
     def not_pure_k1(foo: ti.Template) -> None:
         pass
 
-    stderr_capture.truncate(0)
     not_pure_k1(foo=RandomClass())
-    err = stderr_capture.getvalue()
+    err = redirect_stderr.read()
     assert "FASTCACHE_PARAM_INVALID" not in err
     assert RandomClass.__name__ not in err
     assert "FASTCACHE_INVALID_FUNC" not in err
