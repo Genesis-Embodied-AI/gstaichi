2f4851cb47aeb3c2ffa841f3fe9581e83b9e780a
Hugh Perkins <hughperkins@gmail.com>
2025-08-20 16:42:22 -0400

    capfd, skip windows...

--- tests/python/gstaichi/lang/fast_caching/test_src_ll_cache.py
+++ tests/python/gstaichi/lang/fast_caching/test_src_ll_cache.py
@@ -1,12 +1,9 @@
-import os
 import pathlib
 import sys
-from io import StringIO
 
 import pytest
 
 import gstaichi as ti
-from gstaichi._lib.core import gstaichi_python
 from gstaichi._test_tools import ti_init_same_arch
 
 from tests import test_utils
@@ -44,44 +41,11 @@ def test_src_ll_cache1(tmp_path: pathlib.Path) -> None:
     assert has_pure._primal.src_ll_cache_observations.cache_loaded
 
 
-class StderrCapture:
-    def __init__(self, tmp_file):
-        self.tmp_file = tmp_file
-
-    def read(self):
-        self.tmp_file.flush()
-        self.tmp_file.seek(0)
-        out = self.tmp_file.read()
-        self.tmp_file.truncate(0)
-        self.tmp_file.seek(0)
-        return out
-
-
-@pytest.fixture
-def redirect_stderr(tmp_path):
-    """
-    This redirects the underlying operating system stderr. Since
-    we are calling into a c++ library that does writes to stderr,
-    then redirecting things only in python land is not enough.
-    Concretely, using capfd works on linux, and works for stdout
-    on Windows, but I couldn't get it working with stderr on Windows.
-    """
-    tmp_stderr_path = tmp_path / "stderr.txt"
-    with open(tmp_stderr_path, "w+") as tmp_stderr:
-        old_stderr_fd = os.dup(2)
-        os.dup2(tmp_stderr.fileno(), 2)
-        capture = StderrCapture(tmp_stderr)
-        try:
-            yield capture
-        finally:
-            os.dup2(old_stderr_fd, 2)
-            os.close(old_stderr_fd)
-
-
 # Should be enough to run these on cpu I think, and anything involving
 # stdout/stderr capture is fairly flaky on other arch
 @test_utils.test(arch=ti.cpu)
-def test_src_ll_cache_arg_warnings(tmp_path: pathlib.Path, redirect_stderr) -> None:
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="Windows stderr not working with capfd")
+def test_src_ll_cache_arg_warnings(tmp_path: pathlib.Path, capfd) -> None:
     ti_init_same_arch(offline_cache_file_path=str(tmp_path), offline_cache=True)
 
     class RandomClass:
@@ -93,7 +57,7 @@ def test_src_ll_cache_arg_warnings(tmp_path: pathlib.Path, redirect_stderr) -> N
         pass
 
     k1(foo=RandomClass())
-    err = redirect_stderr.read()
+    _out, err = capfd.readouterr()
     assert "FASTCACHE_PARAM_INVALID" in err
     assert RandomClass.__name__ in err
     assert "FASTCACHE_INVALID_FUNC" in err
@@ -104,7 +68,7 @@ def test_src_ll_cache_arg_warnings(tmp_path: pathlib.Path, redirect_stderr) -> N
         pass
 
     not_pure_k1(foo=RandomClass())
-    err = redirect_stderr.read()
+    _out, err = capfd.readouterr()
     assert "FASTCACHE_PARAM_INVALID" not in err
     assert RandomClass.__name__ not in err
     assert "FASTCACHE_INVALID_FUNC" not in err
