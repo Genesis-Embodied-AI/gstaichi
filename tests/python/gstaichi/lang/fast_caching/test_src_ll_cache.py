7de4582b63a7213ce135aa6bd867c42a3ae78cb9
Hugh Perkins <hughperkins@gmail.com>
2025-08-20 10:29:40 -0400

    add unit test

--- tests/python/gstaichi/lang/fast_caching/test_src_ll_cache.py
+++ tests/python/gstaichi/lang/fast_caching/test_src_ll_cache.py
@@ -1,4 +1,5 @@
 import pathlib
+import pytest
 
 import gstaichi as ti
 from gstaichi._lib.core import gstaichi_python
@@ -37,3 +38,32 @@ def test_src_ll_cache1(tmp_path: pathlib.Path) -> None:
     assert has_pure._primal.src_ll_cache_observations.cache_key_generated
     assert has_pure._primal.src_ll_cache_observations.cache_validated
     assert has_pure._primal.src_ll_cache_observations.cache_loaded
+
+
+@test_utils.test()
+@pytest.mark.parametrize(
+    "flag_value", [
+        None,
+        False,
+        True,
+    ]
+)
+def test_src_ll_cache_flag(tmp_path: pathlib.Path, flag_value: bool) -> None:
+    """
+    Test ti.init(src_ll_cache) flag
+    """
+    if flag_value:
+        ti_init_same_arch(offline_cache_file_path=str(tmp_path), src_ll_cache=flag_value)
+    else:
+        ti_init_same_arch()
+    @ti.pure
+    @ti.kernel
+    def k1() -> None:
+        pass
+
+    k1()
+    cache_used = k1._primal.src_ll_cache_observations.cache_key_generated
+    if flag_value:
+        assert cache_used == flag_value
+    else:
+        assert cache_used  # default
