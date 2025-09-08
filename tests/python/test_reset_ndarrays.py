82796c913f9d2e1bee545b2fa598cd6fbb8906af
Hugh Perkins <hughperkins@gmail.com>
2025-09-08 08:24:47 -0400

    add test of reset

--- tests/python/test_reset_ndarrays.py
+++ tests/python/test_reset_ndarrays.py
@@ -9,7 +9,7 @@ from tests import test_utils
 
 
 @test_utils.test(arch=[ti.cpu])
-def test_ndarray_reset() -> None:
+def test_ndarray_doesnt_crash_on_gc() -> None:
     if sys.platform != "darwin":
         pytest.skip("Only need to check Mac CPU")
     arch = getattr(ti, ti.cfg.arch.name)
@@ -44,3 +44,55 @@ def test_ndarray_reset() -> None:
             assert len(v.arr.shape) > 0, f"{v} it {n}"
         gc.collect()
         k1(z_param)
+
+
+@test_utils.test()
+def test_ndarray_reset() -> None:
+    arch = getattr(ti, ti.cfg.arch.name)
+    ti.reset()
+    ti.init(arch=arch)
+    rt = ti.lang.impl.get_runtime()
+    assert len(rt.ndarrays) == 0
+    a = ti.ndarray(ti.i32, shape=(55,))
+    assert len(rt.ndarrays) == 1
+    b = ti.ndarray(ti.i32, shape=(57,))
+    assert len(rt.ndarrays) == 2
+    c = ti.ndarray(ti.i32, shape=(211,))
+    assert len(rt.ndarrays) == 3
+    z_param = ti.ndarray(ti.i32, shape=(223,))
+    assert len(rt.ndarrays) == 4
+    bar_param = ti.ndarray(ti.i32, shape=(227,))
+    assert len(rt.ndarrays) == 5
+
+    @ti.kernel
+    def k1(z_param2: ti.types.NDArray[ti.i32, 1]) -> None:
+        z_param2[33] += 2
+
+    k1(z_param)
+    assert len(rt.ndarrays) == 5
+
+    ti.reset()
+    rt = ti.lang.impl.get_runtime()
+    assert len(rt.ndarrays) == 0
+
+    assert a.arr is None
+    assert b.arr is None
+    assert c.arr is None
+    assert z_param.arr is None
+    assert bar_param.arr is None
+
+    assert a.shape is None
+
+    gc.collect()
+    gc.collect()
+
+    rt = ti.lang.impl.get_runtime()
+    assert len(rt.ndarrays) == 0
+
+    assert a.arr is None
+    assert b.arr is None
+    assert c.arr is None
+    assert z_param.arr is None
+    assert bar_param.arr is None
+
+    assert a.shape is None
