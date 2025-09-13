5b5bbad4e7213761616507d4c9d35fe256c87a0e
Hugh Perkins <hughperkins@gmail.com>
2025-09-13 06:14:16 -0400

    fix up test_ndarray_type

--- tests/python/pyright/test_ndarray_type.py
+++ tests/python/pyright/test_ndarray_type.py
@@ -1,7 +1,5 @@
 # This is a test file. It just has to exist, to check that pyright works with it.
 
-from typing import Literal
-
 import gstaichi as ti
 
 from tests import test_utils
@@ -10,20 +8,20 @@ ti.init(arch=ti.cpu)
 
 
 @ti.kernel
-def k1(a: ti.types.ndarray(), b: ti.types.NDArray, c: ti.types.NDArray[ti.i32, Literal[1]]) -> None: ...
+def k1(a: ti.types.ndarray(), b: ti.types.NDArray, c: ti.types.NDArray[ti.i32, 1]) -> None: ...
 
 
 @ti.kernel()
-def k2(a: ti.types.ndarray(), b: ti.types.NDArray, c: ti.types.NDArray[ti.i32, Literal[1]]) -> None: ...
+def k2(a: ti.types.ndarray(), b: ti.types.NDArray, c: ti.types.NDArray[ti.i32, 1]) -> None: ...
 
 
 @ti.data_oriented
 class SomeClass:
     @ti.kernel
-    def k1(self, a: ti.types.ndarray(), b: ti.types.NDArray, c: ti.types.NDArray[ti.i32, Literal[1]]) -> None: ...
+    def k1(self, a: ti.types.ndarray(), b: ti.types.NDArray, c: ti.types.NDArray[ti.i32, 1]) -> None: ...
 
     @ti.kernel()
-    def k2(self, a: ti.types.ndarray(), b: ti.types.NDArray, c: ti.types.NDArray[ti.i32, Literal[1]]) -> None: ...
+    def k2(self, a: ti.types.ndarray(), b: ti.types.NDArray, c: ti.types.NDArray[ti.i32, 1]) -> None: ...
 
 
 @test_utils.test()
