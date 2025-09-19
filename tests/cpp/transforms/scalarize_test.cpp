8f2575779584f2a79cd06e9c0316cc5acb9bbeb6
Hugh Perkins <hughperkins@gmail.com>
2025-09-19 13:44:15 -0400

    saving what I ahve now

--- tests/cpp/transforms/scalarize_test.cpp
+++ tests/cpp/transforms/scalarize_test.cpp
@@ -257,24 +257,10 @@ TEST(Scalarize, ScalarizeBugTmp222) {
   auto for2_body = std::make_unique<Block>();
   for2_body->push_back<AllocaStmt>(
       TypeFactory::get_instance().get_primitive_type(PrimitiveTypeID::f32));
-  auto zero_for2 = for2_body->push_back<ConstStmt>(TypedConstant(0));
-  // auto place0 =
-
-  // GlobalLoadStmt(Stmt *src, const DebugInfo &dbg_info = DebugInfo())
-
-  // MatrixOfGlobalPtrStmt(const std::vector<SNode *> &snodes,
-  //                     const std::vector<Stmt *> &indices,
-  //                     bool dynamic_indexable,
-  //                     int dynamic_index_stride,
-  //                     DataType dt,
-  //                     bool activate = true);
 
   block->push_back<RangeForStmt>(zero, one, std::move(for2_body), false, 0, 0,
                                  false);
 
-  // auto offloaded =
-  // std::make_unique<OffloadedStmt>(OffloadedStmt::TaskType::range_for,
-  // Arch::vulkan, kernel.get());
   block->push_back<OffloadedStmt>(OffloadedStmt::TaskType::range_for,
                                   Arch::vulkan, kernel.get());
   auto offloaded = block->statements.back()->as<OffloadedStmt>();
@@ -282,9 +268,7 @@ TEST(Scalarize, ScalarizeBugTmp222) {
 
   auto root_snode = std::make_unique<SNode>(/*depth=*/0, /*t=*/SNodeType::root);
   const std::vector<Axis> axes = {Axis{0}};
-  // ptr_snode_ = &(root_snode_->pointer(axes, kPointerSize));
   auto dense_snode_ = &(root_snode->dense(axes, 1));
-  // Must end with a `place` SNode.
   auto leaf_snode = &(dense_snode_->insert_children(SNodeType::place));
   leaf_snode->dt = PrimitiveType::f32;
   auto leaf_snode2 = &(dense_snode_->insert_children(SNodeType::place));
@@ -294,70 +278,41 @@ TEST(Scalarize, ScalarizeBugTmp222) {
   auto leaf_snode4 = &(dense_snode_->insert_children(SNodeType::place));
   leaf_snode4->dt = PrimitiveType::f32;
 
-  std::vector<int> vector_shape = {4};
+    std::vector<int> vector_shape = {4};
   auto vector_type = TypeFactory::get_instance().get_tensor_type(
       vector_shape,
       TypeFactory::get_instance().get_primitive_type(PrimitiveTypeID::f32));
+  auto pointer_to_vector_type = TypeFactory::get_instance().get_pointer_type(vector_type);
 
+    //   <*[Tensor (4) i32]> $6723 = alloca
+  auto vector_alloca = offloaded->body->push_back<AllocaStmt>(vector_type);
+    // <i32> $6724 = const 0
+      auto zero_for2 = offloaded->body->push_back<ConstStmt>(TypedConstant(0));
+    // <*[Tensor (4) i32]> $6725 = matrix of global ptr [S289place<i32>, S290place<i32>, S291place<i32>, S292place<i32>], index [$6724] activate=true
   std::vector<SNode *> snodes = {leaf_snode, leaf_snode2, leaf_snode3,
                                  leaf_snode4};
   std::vector<Stmt *> indices = {zero_for2};
   auto matrix_global_ptr = offloaded->body->push_back<MatrixOfGlobalPtrStmt>(
-      snodes, indices, false, 1, vector_type, true);
-  // auto matrix_global_ptr =
-  // offloaded->body->push_back<MatrixOfGlobalPtrStmt>(snodes, indices, false,
-  // 1, PrimitiveType::f32, true);
+      snodes, indices, false, 1, pointer_to_vector_type, true);
+    // <[Tensor (4) i32]> $6726 = global load $6725
   auto global_load =
       offloaded->body->push_back<GlobalLoadStmt>(matrix_global_ptr);
-
-  auto vector_alloca = offloaded->body->push_back<AllocaStmt>(vector_type);
-
-  // offloaded->body->push_back<AllocaStmt>();
+    // $6727 : local store [$6723 <- $6726]
   offloaded->body->push_back<LocalStoreStmt>(vector_alloca, global_load);
+    // <*[Tensor (4) i32]> $6728 = alloca
+    auto vector_alloca2 = offloaded->body->push_back<AllocaStmt>(vector_type);
+    // <[Tensor (4) i32]> $6729 = local load [$6723]
+  auto vector_load2 = offloaded->body->push_back<LocalLoadStmt>(vector_alloca);
+    // $6730 : local store [$6728 <- $6729]
+  offloaded->body->push_back<LocalStoreStmt>(vector_alloca2, vector_load2);
 
-  // RangeForStmt(Stmt *begin,
-  //   Stmt *end,
-  //   std::unique_ptr<Block> &&body,
-  //   bool is_bit_vectorized,
-  //   int num_cpu_threads,
-  //   int block_dim,
-  //   bool strictly_serialized,
-  //   std::string range_hint = "");
 
-  // auto &type_factory = TypeFactory::get_instance();
 
   irpass::print(block.get());
-
-  // /*
-  //   TensorType<4 x i32>* %1 = AllocaStmt()
-  //   LoadStmt(%1)
-  // */
-  // Type *tensor_type = type_factory.get_tensor_type(
-  //     {2, 2}, type_factory.get_primitive_type(PrimitiveTypeID::i32));
-  // Stmt *src_stmt = block->push_back<AllocaStmt>(tensor_type);
-  // src_stmt->ret_type = type_factory.get_pointer_type(tensor_type);
-
-  // auto load_stmt = block->push_back<LocalLoadStmt>(src_stmt);
-
-  // // Without this GlobalStoreStmt, nothing survives irpass::die()
-  // block->push_back<GlobalStoreStmt>(src_stmt, load_stmt);
 
   irpass::scalarize(block.get());
   irpass::print(block.get());
-  // irpass::die(block.get());
 
-  // EXPECT_EQ(block->size(), 4 /*alloca*/ + 4 /*load*/ + 4 /*store*/);
-
-  // // Check for scalarized statements
-  // EXPECT_EQ(block->statements[0]->is<AllocaStmt>(), true);
-  // EXPECT_EQ(block->statements[1]->is<AllocaStmt>(), true);
-  // EXPECT_EQ(block->statements[2]->is<AllocaStmt>(), true);
-  // EXPECT_EQ(block->statements[3]->is<AllocaStmt>(), true);
-
-  // EXPECT_EQ(block->statements[4]->is<LocalLoadStmt>(), true);
-  // EXPECT_EQ(block->statements[5]->is<LocalLoadStmt>(), true);
-  // EXPECT_EQ(block->statements[6]->is<LocalLoadStmt>(), true);
-  // EXPECT_EQ(block->statements[7]->is<LocalLoadStmt>(), true);
 }
 
 }  // namespace gstaichi::lang
