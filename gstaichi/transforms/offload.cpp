a17e47af9973923f70f0b75d1fb9b0a047fb59ba
Hugh Perkins <hughperkins@gmail.com>
2025-09-19 08:34:00 -0400

    precommit

--- gstaichi/transforms/offload.cpp
+++ gstaichi/transforms/offload.cpp
@@ -350,8 +350,8 @@ class IdentifyValuesUsedInOtherOffloads : public BasicStmtVisitor {
       std::size_t type_size = data_type_size(type);
       // align global_offset to a multiple of type_size
       // note that our current SPIR-V codegen requires at least 4 byte alignment
-      // TODO: is this a theoretical constraint of SPIR-V, or is this a bug/feature
-      // of our SPIR-V codegen?
+      // TODO: is this a theoretical constraint of SPIR-V, or is this a
+      // bug/feature of our SPIR-V codegen?
       type_size = std::max(type_size, (std::size_t)4);
       global_offset_ =
           ((global_offset_ + type_size - 1) / type_size) * type_size;
