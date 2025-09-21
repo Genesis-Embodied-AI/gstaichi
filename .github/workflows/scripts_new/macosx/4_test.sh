751aa4328a68b113b257e9730ed6916ac14f14cd
Hugh Perkins <hughperkins@gmail.com>
2025-09-21 19:14:45 -0400

    whoops, added  --arch metal,vulkan,cpu to win instead of mac 😅

--- .github/workflows/scripts_new/macosx/4_test.sh
+++ .github/workflows/scripts_new/macosx/4_test.sh
@@ -9,4 +9,4 @@ ls -lh build/
 export TI_LIB_DIR="$(python -c 'import gstaichi as ti; print(ti.__path__[0])' | tail -n 1)/_lib/runtime"
 chmod +x ./build/gstaichi_cpp_tests
 ./build/gstaichi_cpp_tests
-python tests/run_tests.py -v -r 3
+python tests/run_tests.py -v -r 3 --arch metal,vulkan,cpu
