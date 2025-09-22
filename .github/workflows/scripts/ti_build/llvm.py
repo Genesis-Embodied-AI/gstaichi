e4ca80c1cd39d2323854dc1852aa4a4d5c1cad1d
Hugh Perkins <hughperkins@gmail.com>
2025-09-21 20:56:54 -0400

    llvm18

--- .github/workflows/scripts/ti_build/llvm.py
+++ .github/workflows/scripts/ti_build/llvm.py
@@ -20,22 +20,22 @@ def setup_llvm() -> None:
     """
     u = platform.uname()
 
-    release_url_template = "https://github.com/Genesis-Embodied-AI/gstaichi-sdk-builds/releases/download/llvm-16.0.6-hp-llvm-u18-container-202509212058/taichi-llvm-16.0.6-{platform}.zip"
+    release_url_template = "https://github.com/Genesis-Embodied-AI/gstaichi-sdk-builds/releases/download/llvm-18.1.8-hp-llvm-u18-container-202509220042/taichi-llvm-18.1.8-{platform}.zip"
 
     if u.system == "Linux":
         if cmake_args.get_effective("TI_WITH_AMDGPU"):
-            out = get_cache_home() / "llvm16-amdgpu-005"
+            out = get_cache_home() / "llvm18-amdgpu-005"
             url = "https://github.com/GaleSeLee/assets/releases/download/v0.0.5/taichi-llvm-15.0.0-linux.zip"
         else:
-            out = get_cache_home() / "llvm16-x86"
+            out = get_cache_home() / "llvm18-x86"
             url = release_url_template.format(platform="linux-x86_64")
         download_dep(url, out, strip=1)
     elif (u.system, u.machine) == ("Darwin", "arm64"):
-        out = get_cache_home() / "llvm16"
+        out = get_cache_home() / "llvm18"
         url = release_url_template.format(platform="macos-arm64")
         download_dep(url, out, strip=1)
     elif (u.system, u.machine) == ("Windows", "AMD64"):
-        out = get_cache_home() / "llvm16"
+        out = get_cache_home() / "llvm18"
         url = release_url_template.format(platform="windows-amd64")
         download_dep(url, out, strip=0)
     else:
