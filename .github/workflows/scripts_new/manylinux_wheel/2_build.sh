e84ec831e963697b7af741f25f059b45043f0d5d
Hugh Perkins <hughperkins@gmail.com>
2025-10-20 13:40:54 -0400

    bit of diag

--- .github/workflows/scripts_new/manylinux_wheel/2_build.sh
+++ .github/workflows/scripts_new/manylinux_wheel/2_build.sh
@@ -5,6 +5,8 @@ set -ex
 # this was already downloaded in 1_prerequisites.sh, so this is just to set the env var
 LLVM_DIR=$(python download_llvm.py)
 export PATH=${LLVM_DIR}/bin:$PATH
+which clang
+clang --version
 
 export GSTAICHI_CMAKE_ARGS="-DTI_WITH_VULKAN:BOOL=ON -DTI_WITH_CUDA:BOOL=ON -DTI_BUILD_TESTS:BOOL=ON"
 ./build.py wheel
