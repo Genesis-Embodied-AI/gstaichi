dee64f02c848c4054ba58c757294ad8791f44875
Hugh Perkins <hughperkins@gmail.com>
2025-10-21 15:08:44 -0400

    placate pyright

--- python/gstaichi/lang/_fast_caching/args_hasher.py
+++ python/gstaichi/lang/_fast_caching/args_hasher.py
@@ -89,8 +89,13 @@ def stringify_obj_type(
         return dataclass_to_repr(raise_on_templated_floats, path, obj)
     if is_data_oriented(obj):
         child_repr_l = ["da"]
-        # _asdict() is for NamedTuple's
-        _dict = obj.__dict__ if hasattr(obj, "__dict__") else obj._asdict()
+        _dict = {}
+        try:
+            # pyright is ok with this approach
+            _asdict = getattr(obj, "_asdict")
+            _dict = _asdict()
+        except AttributeError:
+            _dict = obj.__dict__
         for k, v in _dict.items():
             _child_repr = stringify_obj_type(raise_on_templated_floats, (*path, k), v, ArgMetadata(Template, ""))
             if _child_repr is None:
