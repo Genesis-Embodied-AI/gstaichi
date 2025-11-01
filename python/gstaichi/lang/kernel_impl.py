d818c843db1ba5a2ca3e8355d515c7820f4f242c
Hugh Perkins <hughperkins@gmail.com>
2025-11-01 18:06:45 +0800

    slight rearrangment of field name

--- python/gstaichi/lang/kernel_impl.py
+++ python/gstaichi/lang/kernel_impl.py
@@ -731,10 +731,10 @@ def _recursive_set_args(
         idx = 0
         offset = indices[0]
         for field in needed_arg_fields.values():
-            field_name = field.name
-            field_full_name = f"{py_dataclass_basename}.{field_name}"
             if field._field_type is not _FIELD:
                 continue
+            field_name = field.name
+            field_full_name = py_dataclass_basename + "." + field_name
             if field_full_name not in used_py_dataclass_parameters:
                 continue
             # Storing attribute in a temporary to avoid repeated attribute lookup (~20ns penalty)
