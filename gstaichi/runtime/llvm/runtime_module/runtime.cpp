245ce28e0929470b6a2bb398931a426cad27a679
Hugh Perkins <hughperkins@gmail.com>
2025-11-05 18:00:03 -0500

    linux and aarch64

--- gstaichi/runtime/llvm/runtime_module/runtime.cpp
+++ gstaichi/runtime/llvm/runtime_module/runtime.cpp
@@ -54,8 +54,7 @@ __asm__(".symver powf,powf@GLIBC_2.2.5");
 __asm__(".symver expf,expf@GLIBC_2.2.5");
 #endif
 
-#if (defined(__aarch64__) || defined(__arm64__)) && defined(__clang__) && \
-    defined(__linux___)
+#if (defined(__linux___) && defined(__aarch64__) && defined(__clang__))
 // JIT session error: Symbols not found: [ __aarch64_ldadd4_acq_rel, ... ]
 // This is an issue with newer clang versions (>13) on aarch64, where
 // atomics are outlined by default. The JIT environment does not
