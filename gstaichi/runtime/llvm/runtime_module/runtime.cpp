97ae10c4c6ff962d5ac1a8760fdaa84ec0f4b7bd
Hugh Perkins <hughperkins@gmail.com>
2025-11-05 14:48:33 -0500

    Revert "defined linux"
    This reverts commit 8b2ab091252871a3fb29a8b0f05205942c89f243.

--- gstaichi/runtime/llvm/runtime_module/runtime.cpp
+++ gstaichi/runtime/llvm/runtime_module/runtime.cpp
@@ -55,7 +55,7 @@ __asm__(".symver expf,expf@GLIBC_2.2.5");
 #endif
 
 #if (defined(__aarch64__) || defined(__arm64__)) && defined(__clang__) && \
-    defined(__linux___)
+    !defined(__APPLE__)
 // JIT session error: Symbols not found: [ __aarch64_ldadd4_acq_rel, ... ]
 // This is an issue with newer clang versions (>13) on aarch64, where
 // atomics are outlined by default. The JIT environment does not
