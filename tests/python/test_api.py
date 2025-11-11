928ea22a671fb0c93ad635c15a5b4c57f1ab5dae
Hugh Perkins <hughperkins@gmail.com>
2025-11-11 18:06:53 -0500

    test_api

--- tests/python/test_api.py
+++ tests/python/test_api.py
@@ -359,9 +359,10 @@ user_api[ti.MatrixNdarray] = [
     "fill",
     "from_numpy",
     "get_type",
+    "to_dlpack",
     "to_numpy",
 ]
-user_api[ti.Ndarray] = ["copy_from", "element_shape", "fill", "get_type"]
+user_api[ti.Ndarray] = ["copy_from", "element_shape", "fill", "get_type", "to_dlpack"]
 user_api[ti.Texture] = ["from_field", "from_image", "from_ndarray", "to_image"]
 user_api[ti.SNode] = [
     "bitmasked",
@@ -394,6 +395,7 @@ user_api[ti.ScalarNdarray] = [
     "fill",
     "from_numpy",
     "get_type",
+    "to_dlpack",
     "to_numpy",
 ]
 user_api[ti.Struct] = ["entries", "field", "items", "keys", "methods", "to_dict"]
@@ -417,6 +419,7 @@ user_api[ti.VectorNdarray] = [
     "fill",
     "from_numpy",
     "get_type",
+    "to_dlpack",
     "to_numpy",
 ]
 user_api[ti.sparse] = ["grid", "usage"]
