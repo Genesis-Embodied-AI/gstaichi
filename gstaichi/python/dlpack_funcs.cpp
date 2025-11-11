33cc47f19dfabe6f57a6c647fb9bbb0926214c27
Hugh Perkins <hughperkins@gmail.com>
2025-11-11 11:07:27 -0500

    created a cpu torch tensor from ndarray

--- gstaichi/python/dlpack_funcs.cpp
+++ gstaichi/python/dlpack_funcs.cpp
@@ -19,11 +19,13 @@ pybind11::capsule dlpack_dump_ndarray_info(Program *program, Ndarray *ndarray) {
   DeviceAllocation devalloc = ndarray->get_device_allocation();
 
   cpu::CpuDevice *cpu_device = dynamic_cast<cpu::CpuDevice *>(devalloc.device);
+  void *raw_ptr = nullptr;
+  DLDeviceType device_type = DLDeviceType::kDLCPU;
   if(cpu_device != nullptr) {
     std::cout << "cpu_device not nullptr " << (cpu_device != nullptr) << std::endl;
     // std::cout << " dev_alloc.ptr " << devalloc.ptr << std::endl;
     cpu::CpuDevice::AllocInfo alloc_info = cpu_device->get_alloc_info(devalloc);
-    auto raw_ptr = alloc_info.ptr;
+    raw_ptr = alloc_info.ptr;
     std::cout << " dev_alloc.ptr " << raw_ptr << std::endl;
     std::cout << ((int *)raw_ptr)[0] << std::endl;
     std::cout << ((int *)raw_ptr)[1] << std::endl;
@@ -33,10 +35,11 @@ pybind11::capsule dlpack_dump_ndarray_info(Program *program, Ndarray *ndarray) {
   cuda::CudaDevice *cuda_device = dynamic_cast<cuda::CudaDevice *>(devalloc.device);
   if(cuda_device != nullptr) {
     std::cout << "cuda_device not nullptr " << (cuda_device != nullptr) << std::endl;
+    cuda::CudaDevice::AllocInfo alloc_info = cuda_device->get_alloc_info(devalloc);
+    raw_ptr = alloc_info.ptr;
+    device_type = DLDeviceType::kDLCUDA;
+    std::cout << " dev_alloc.ptr " << raw_ptr << std::endl;
   }
-  cuda::CudaDevice::AllocInfo alloc_info = cuda_device->get_alloc_info(devalloc);
-  auto raw_ptr = alloc_info.ptr;
-  std::cout << " dev_alloc.ptr " << raw_ptr << std::endl;
 #endif // TI_WITH_CUDA
 
     MyData *my_data = new MyData;
@@ -44,15 +47,46 @@ pybind11::capsule dlpack_dump_ndarray_info(Program *program, Ndarray *ndarray) {
 
     // return my_data;
 
-    // DLTensor dl_tensor;
-    // dl_tensor.data = reinterpret_cast<void *>(devalloc.ptr);
+    int64_t *shape = new int64_t[2];
+    shape[0] = 10;
+    shape[1] = 3;
+    int64_t *strides = new int64_t[2];
+    strides[0] = 3;
+    strides[1] = 1;
 
+    // DLManagedTensorVersioned *managed_tensor = new DLManagedTensorVersioned;
+    DLManagedTensor *managed_tensor = new DLManagedTensor();
+
+    DLTensor &dl_tensor = managed_tensor->dl_tensor;
+    dl_tensor.data = raw_ptr;
+    dl_tensor.device.device_type = device_type;
+    dl_tensor.device.device_id = 0;
+    dl_tensor.ndim = 2;
+    dl_tensor.dtype = DLDataType{kDLInt, 32, 1};
+    dl_tensor.shape = shape;
+    dl_tensor.strides = strides;
+    dl_tensor.byte_offset = 0;
+
+    std::cout << " device type " << device_type << " " << managed_tensor->dl_tensor.device.device_type <<std::endl;
+
+    // managed_tensor->dl_tensor = dl_tensor;
+    managed_tensor->manager_ctx = ndarray;
+    managed_tensor->deleter = [](DLManagedTensor *self) {
+        // MyData *my_data = static_cast<MyData *>(self->manager_ctx);
+        // delete my_data;
+        delete[] self->dl_tensor.shape;
+        delete[] self->dl_tensor.strides;
+        delete self;
+    };
     auto deleter = [](PyObject *capsule) {
-        MyData *my_data = static_cast<MyData *>(PyCapsule_GetPointer(capsule, "my_data"));
-        delete my_data;
+        // MyData *my_data = static_cast<MyData *>(PyCapsule_GetPointer(capsule, "my_data"));
+        // delete my_data;
     };
 
-    pybind11::capsule capsule = pybind11::capsule(static_cast<void *>(my_data), "my_data", deleter);
+    // ndarray->inc_ref();
+
+    // pybind11::capsule capsule = pybind11::capsule(static_cast<void *>(my_data), "my_data", deleter);
+    pybind11::capsule capsule = pybind11::capsule(static_cast<void *>(managed_tensor), "dltensor", deleter);
     return capsule;
 }
 }
