e4402f6d3d7b1410fe2b47cedd3a8581d3c78d0f
Hugh Perkins <hughperkins@gmail.com>
2025-11-12 12:30:23 -0500

    remove dead code, and add cuda

--- gstaichi/python/dlpack_funcs.cpp
+++ gstaichi/python/dlpack_funcs.cpp
@@ -18,28 +18,8 @@ namespace lang {
 pybind11::capsule field_to_dlpack(Program *program,
                                     pybind11::object owner,
                                     SNode *snode) {
-  // std::cout << "snode " << snode << " id " << snode->id << " depth " << snode->depth << " name " << snode->name << " chunk_size " << snode->chunk_size <<
-    //  " cell_size_bytes " << snode->cell_size_bytes << " offset_bytes_in_parent_cell " << snode->offset_bytes_in_parent_cell <<
-    //  " is_path_all_dense " << snode->is_path_all_dense << " index_offsets.size() " << snode->index_offsets.size() << 
-    //  " num_active_indices " << snode->num_active_indices << " num_cells_per_container " << snode->num_cells_per_container <<
-    //  " ch.size() " << snode->ch.size() << " snode_tree_id " << snode->get_snode_tree_id() << std::endl;
-  // for(int i=0; i < snode->index_offsets.size(); i++) {
-  //   std::cout << "  index_offsets[" << i << "] = " << snode->index_offsets[i] << std::endl;
-  // }
-  // for(int i=0; i < snode->num_active_indices; i++) {
-  //   std::cout << "  physical_index_position[" << i << "] = " << snode->physical_index_position[i] << std::endl;
-  // }
-  // for(int i=0; i < snode->num_active_indices; i++) {
-  //   AxisExtractor &extractor = snode->extractors[i];
-  //   std::cout << "  extractor[" << i << "] active " << extractor.active << " num_elements_from_root " << extractor.num_elements_from_root <<
-  //   " shape " << extractor.shape << " acc_shape " << extractor.acc_shape << std::endl;
-  // }
-
   int tree_id = snode->get_snode_tree_id();
   DevicePtr tree_device_ptr = program->get_snode_tree_device_ptr(tree_id);
-  // std::cout << "tree_device_ptr " << (void *)&tree_device_ptr << " offset " << tree_device_ptr.offset << std::endl;
-  // int field_in_tree_offset = program->get_field_in_tree_offset(tree_id, snode);
-  // std::cout << "field_in_tree_offset " << field_in_tree_offset << std::endl;
 
   DataType dt = snode->dt;
   PrimitiveTypeID type_id = dt->as<PrimitiveType>()->type;
@@ -105,8 +85,17 @@ pybind11::capsule field_to_dlpack(Program *program,
     device_type = DLDeviceType::kDLCPU;
     cpu::CpuDevice::AllocInfo alloc_info = cpu_device->get_alloc_info(tree_device_ptr);
     raw_ptr = alloc_info.ptr;
-    // int *ptr_as_int = (int *)raw_ptr;
   }
+#if TI_WITH_CUDA
+  else if (arch_is_cuda(arch)) {
+    cuda::CudaDevice *cuda_device =
+        static_cast<cuda::CudaDevice *>(tree_device_ptr.device);
+    device_type = DLDeviceType::kDLCUDA;
+    cuda::CudaDevice::AllocInfo alloc_info =
+        cuda_device->get_alloc_info(tree_device_ptr);
+    raw_ptr = alloc_info.ptr;
+  }
+#endif  // TI_WITH_CUDA
 
 
   DLManagedTensor *managed_tensor = new DLManagedTensor();
