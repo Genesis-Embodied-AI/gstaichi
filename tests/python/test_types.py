fc17f8ed0a27a52dfa5eb06c966128ef3a23aee4
Hugh Perkins <hughperkins@gmail.com>
2025-11-26 14:07:54 -0500

    test_types_fields_and_dtypes_np_read_write_np_consistency

--- tests/python/test_types.py
+++ tests/python/test_types.py
@@ -230,3 +230,50 @@ def test_types_fields_and_dtypes_kernel_from_numpy_to_numpy_consistency(tensor_t
 
     for i in range(16):
         assert b_np[i] == (1 if i in poses else 0)
+
+
+@pytest.mark.parametrize("tensor_type", [ti.field, ti.ndarray])
+@pytest.mark.parametrize("dtype", [ti.u1, ti.u8, ti.u16, ti.u32, ti.u64, ti.i8, ti.i32, ti.i16, ti.i64])
+@test_utils.test()
+def test_types_fields_and_dtypes_np_read_write_np_consistency(tensor_type, dtype) -> None:
+    """
+    numpy => read from kernel => write from kernel => numpy
+    check consistency
+    """
+    poses = [0, 2, 5, 11]
+
+    ti_dtype_to_np_dtype = {
+        ti.u1: np.bool_,
+        ti.u8: np.uint8,
+        ti.u16: np.uint16,
+        ti.u32: np.uint32,
+        ti.u64: np.uint64,
+        ti.i8: np.int8,
+        ti.i16: np.int16,
+        ti.i32: np.int32,
+        ti.i64: np.int64
+    }
+    np_dtype = ti_dtype_to_np_dtype[dtype]
+    
+    a_np = np.zeros(dtype=np_dtype, shape=(16,))
+    a = tensor_type(dtype, (16,))
+    b = tensor_type(dtype, (16,))
+
+    for pos in poses:
+        a_np[pos] = 1
+    a.from_numpy(a_np)
+
+    TensorType = ti.types.NDArray if tensor_type == ti.ndarray else ti.Template
+
+    @ti.kernel
+    def k1(a: TensorType, b: TensorType) -> None:
+        for b_ in range(1):
+            for pos in ti.static(poses):
+                b[pos] = a[pos]
+
+    k1(a, b)
+
+    b_np = b.to_numpy()
+
+    for i in range(16):
+        assert b_np[i] == (1 if i in poses else 0)
