eb364e97c1d3131a2a7ce0484875132c577ff50a
Hugh Perkins <hughperkins@gmail.com>
2025-12-01 14:21:39 -0500

    add uv to path for windows

--- .github/workflows/scripts_new/win/1_prerequisites.ps1
+++ .github/workflows/scripts_new/win/1_prerequisites.ps1
@@ -3,6 +3,7 @@ Set-PSDebug -Trace 1
 trap { Write-Error $_; exit 1 }
 
 powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+$env:Path = "$HOME\.local\bin;$env:Path"
 uv venv
 
 # This will install Visual Studio Build Tools, then exit, with an (intentional) exception:
