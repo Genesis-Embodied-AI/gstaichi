7fcf74b22d1c53faaaee5911eff177e1210149fb
Hugh Perkins <hughperkins@gmail.com>
2025-12-01 15:42:07 -0500

    $env:Path = "$HOME\.local\bin;$env:Path" for windows build

--- .github/workflows/scripts_new/win/2_build.ps1
+++ .github/workflows/scripts_new/win/2_build.ps1
@@ -2,5 +2,7 @@ $ErrorActionPreference = "Stop"
 Set-PSDebug -Trace 1
 trap { Write-Error $_; exit 1 }
 
+# for uv
+$env:Path = "$HOME\.local\bin;$env:Path"
 $env:GSTAICHI_CMAKE_ARGS = "-DTI_WITH_VULKAN:BOOL=ON -DTI_WITH_CUDA:BOOL=ON -DTI_BUILD_TESTS:BOOL=ON"
 python build.py
