7ae907a5aac38f9900b190e218e1b2bdd5ec966e
Alexis Duburcq <alexis.duburcq@gmail.com>
2025-12-09 20:32:12 -0800

    Add documentation.

--- python/gstaichi/lang/simt/timer.py
+++ python/gstaichi/lang/simt/timer.py
@@ -4,6 +4,12 @@ from gstaichi.lang import impl
 
 
 def cuda_clock_i64():
+    """
+    Returns the value of a per-multiprocessor counter that is incremented every clock cycle.
+
+    See official documentation for details:
+    https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#time-function
+    """
     return impl.call_internal("cuda_clock_i64", with_runtime_context=False)
 
 
