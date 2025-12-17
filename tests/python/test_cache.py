4f3234fc45fbd3c716b997b63fb0aa1cfd8fa2e5
Alexis Duburcq <alexis.duburcq@gmail.com>
2025-12-17 16:21:48 +0100

    More unit tests.

--- tests/python/test_cache.py
+++ tests/python/test_cache.py
@@ -5,7 +5,7 @@ from tests import test_utils
 
 
 @test_utils.test(arch=get_host_arch_list())
-def test_cache():
+def test_cache_primitive_args():
     @ti.data_oriented
     class StructStaticConfig:
         flag_1: bool = True
@@ -34,6 +34,33 @@ def test_cache():
     assert len(fun._primal._launch_ctx_cache_tracker) == 1
 
     fun(static_args, flag_2, value)
+    assert value[None] == 3
+    assert len(fun._primal.mapper.mapping) == 1
+    assert len(fun._primal._launch_ctx_cache_tracker) == 1
+
+
+@test_utils.test(arch=get_host_arch_list())
+def test_cache_fields_only():
+    @ti.kernel
+    def fun(flag: ti.template(), value: ti.template()):
+        if ti.static(flag):
+            value[None] = value[None] + 1
+        else:
+            assert "Invalid 'static_args.flag_1' branch"
+
+    assert len(fun._primal.mapper.mapping) == 0
+    assert len(fun._primal._launch_ctx_cache_tracker) == 0
+
+    flag = True
+    value = ti.field(ti.i32, shape=())
+    value[None] = 1
+
+    fun(flag, value)
+    assert value[None] == 2
+    assert len(fun._primal.mapper.mapping) == 1
+    assert len(fun._primal._launch_ctx_cache_tracker) == 1
+
+    fun(flag, value)
     assert value[None] == 3
     assert len(fun._primal.mapper.mapping) == 1
     assert len(fun._primal._launch_ctx_cache_tracker) == 1
