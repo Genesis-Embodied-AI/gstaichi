d2e72ce1d541f93b3685428ca13e8977a095b3d7
Hugh Perkins <hughperkins@gmail.com>
2025-12-17 23:35:10 -0500

    ir_dump_dir

--- gstaichi/transforms/simplify.cpp
+++ gstaichi/transforms/simplify.cpp
@@ -537,8 +537,9 @@ void full_simplify(IRNode *root,
   bool should_dump =
       (dump_simplify_env && std::string(dump_simplify_env) == "1");
   auto dump_step = [&](const std::string &step_name, int iteration) {
+    std::filesystem::path ir_dump_dir = config.debug_dump_path;
     std::filesystem::path filename =
-        IR_DUMP_DIR /
+        ir_dump_dir /
         (args.kernel_name + ".simplify_I_call" + std::to_string(this_call) +
          "_iter" + std::to_string(iteration) + "_" + step_name + ".ir");
     std::string ir_str;
