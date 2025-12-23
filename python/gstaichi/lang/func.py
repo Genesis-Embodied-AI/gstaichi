92824a3b2d72265ee207e9af4d933bf869d368ff
Hugh Perkins <hughperkins@gmail.com>
2025-12-23 15:54:36 -0500

    pyrigth

--- python/gstaichi/lang/func.py
+++ python/gstaichi/lang/func.py
@@ -55,6 +55,7 @@ class Func(FuncBase):
         runtime = impl.get_runtime()
         global_context = runtime._current_global_context
         current_kernel = global_context.current_kernel if global_context is not None else None
+        assert current_kernel is not None
         py_args = self.process_args(
             is_func=True, is_pyfunc=self.pyfunc, py_args=py_args, kwargs=kwargs, global_context=global_context
         )
@@ -65,7 +66,6 @@ class Func(FuncBase):
             return self.func(*py_args)
 
         if self.is_real_function:
-            assert current_kernel is not None
             if current_kernel.autodiff_mode != _NONE:
                 raise GsTaichiSyntaxError("Real function in gradient kernels unsupported.")
             instance_id, arg_features = self.mapper.lookup(impl.current_cfg().raise_on_templated_floats, py_args)
