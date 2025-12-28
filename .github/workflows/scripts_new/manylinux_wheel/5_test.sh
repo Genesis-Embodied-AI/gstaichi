b7184efb5a5e3283f5732109c52c44e79e076075
Hugh Perkins <hughperkins@gmail.com>
2025-12-28 13:00:13 -0500

    use torch ngihtly for all os

--- .github/workflows/scripts_new/manylinux_wheel/5_test.sh
+++ .github/workflows/scripts_new/manylinux_wheel/5_test.sh
@@ -4,5 +4,6 @@ set -ex
 
 pip install --group test
 pip install -r requirements_test_xdist.txt
-pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+# TODO: revert to stable torch after 2.9.2 release
+pip install --pre --upgrade torch --index-url https://download.pytorch.org/whl/nightly/cpu
 python tests/run_tests.py -v -r 3
--- .github/workflows/scripts_new/win/3_test.ps1
+++ .github/workflows/scripts_new/win/3_test.ps1
@@ -7,5 +7,6 @@ $env:TI_LIB_DIR="python/gstaichi/_lib/runtime"
 Get-ChildItem -Path build -Recurse
 pip install --group test
 pip install -r requirements_test_xdist.txt
-pip install torch
+# TODO: revert to stable torch after 2.9.2 release
+pip install --pre --upgrade torch --index-url https://download.pytorch.org/whl/nightly/cpu
 python .\tests\run_tests.py -v -r 3
