435476381aeb7fa102d8bdc38b44d1775adfedc3
Hugh Perkins <hughperkins@gmail.com>
2026-01-13 09:32:36 -0500

    enable data oriented

--- python/gstaichi/lang/_pruning.py
+++ python/gstaichi/lang/_pruning.py
@@ -69,10 +69,15 @@ class Pruning:
         # because of the way calling with sequential args works.
         # We need to look at the child's declaration - via metas - in order to get the name they use.
         # We can't tell their name just by looking at our own metas.
+        #
+        # One issue is when calling data-oriented methods, there will be a `self`. We'll detect this
+        # by seeing if the childs arg_metas_expanded is exactly 1 longer than len(node.args) + len(node.kwargs)
+        has_self = len(node.args) + len(node.keywords) + 1 == len(node.func.ptr.wrapper.arg_metas_expanded)
+        self_offset = 1 if has_self else 0
         for i, arg in enumerate(node.args):
             if type(arg) in {Name}:
                 caller_arg_name = arg.id  # type: ignore
-                callee_param_name = node.func.ptr.wrapper.arg_metas_expanded[arg_id].name  # type: ignore
+                callee_param_name = node.func.ptr.wrapper.arg_metas_expanded[arg_id + self_offset].name  # type: ignore
                 if callee_param_name in callee_used_vars:
                     vars_to_unprune.add(caller_arg_name)
             arg_id += 1
@@ -99,7 +104,7 @@ class Pruning:
             if type(arg) in {Name}:
                 caller_arg_name = arg.id  # type: ignore
                 if caller_arg_name.startswith("__ti_"):
-                    callee_param_name = child_metas[child_arg_id].name
+                    callee_param_name = child_metas[child_arg_id + self_offset].name
                     if callee_param_name in used_callee_vars or not callee_param_name.startswith("__ti_"):
                         callee_param_by_called_arg_name[caller_arg_name] = callee_param_name
             child_arg_id += 1
