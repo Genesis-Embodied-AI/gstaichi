68d255e8334f5a2734b9cef6a6a3c401c8759288
Hugh Perkins <hughperkins@gmail.com>
2026-01-14 08:35:25 -0500

    split 1-liner

--- python/gstaichi/lang/_exceptions.py
+++ python/gstaichi/lang/_exceptions.py
@@ -5,4 +5,6 @@ def raise_exception(
     ExceptionClass: Type[Exception], msg: str, category: str, err_code: str, orig: Exception | None = None
 ):
     err = ExceptionClass(f"[{category}] {msg} (error code: {err_code}).")
-    raise err from orig if orig else err
+    if orig:
+        raise err from orig
+    raise err
