b8b35313a7b77ec8d73a05bbb500f16c09b7abaf
Hugh Perkins <hughperkins@gmail.com>
2026-01-14 07:06:00 -0500

    precommit

--- python/gstaichi/lang/_exceptions.py
+++ python/gstaichi/lang/_exceptions.py
@@ -1,6 +1,8 @@
 from typing import Type
 
 
-def raise_exception(ExceptionClass: Type[Exception], msg: str, category: str, err_code: str, orig: Exception | None = None):
+def raise_exception(
+    ExceptionClass: Type[Exception], msg: str, category: str, err_code: str, orig: Exception | None = None
+):
     err = ExceptionClass(f"[{category}] {msg} (error code: {err_code}).")
     raise err from orig if orig else err
